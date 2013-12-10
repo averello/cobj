f86cb666ccac942aab37c764a0b6c7636303f4b0
George Boumis <george.boumis@gmail.com>
2013-12-10 02:30:51 +0100

    Introduce foreach in the library.
    Introduce Collection abstract class.

--- test/testFastEnumeration.c
+++ test/testFastEnumeration.c
@@ -19,32 +19,9 @@ int main () {
 			release(firstString);
 		}
 		
-		{
-			FastEnumerationState ___state;
-			__builtin___memset_chk (&___state, 0, sizeof(FastEnumerationState), __builtin_object_size (&___state, 0));
-			ObjectRef ___buffer[16];
-			memset(___buffer, 0, sizeof(___buffer));
-			int ___firstLoop = 1;
-			long ___mutationsPointerValue;
-			unsigned long ___count;
-			while ((___count = enumerateWithState((strings), &___state, ___buffer, 16))) {
-				if (!___firstLoop && ___mutationsPointerValue != *___state.mutationsPointer) {
-					(__builtin_expect(!(0 && "Object mutated while iterating"), 0) ? __assert_rtn(__func__, "/Volumes/Macintosh HD/Users/averello/Programing/C Programing/cobj/test/testFastEnumeration.c", 21, "0 && \"Object mutated while iterating\"") : (void)0);
-				}
-				___firstLoop = 0;
-				___mutationsPointerValue = *___state.mutationsPointer;
-				StringRef string;
-				for (unsigned long ___index=0; ___index<___count; ___index++) {
-					string = ___state.itemsPointer[___index]; {
-						puts(getStringText(string));
-					}
-				}
-			}
-		}
-		
-//		foreach_start(StringRef, string, strings) {
-//			 puts(getStringText(string));
-//		} foreach_end()
+		foreach_start(StringRef, string, strings) {
+			 puts(getStringText(string));
+		} foreach_end()
 		
 		release(strings);
 	}
